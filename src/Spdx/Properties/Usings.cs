5fb69f7d5169fbaf53cdb2089708c334baa06f0a
Patrik Svensson <patrik@patriksvensson.se>
2022-07-03 12:04:07 +0200

    Update dependencies

--- src/Spdx/Properties/Usings.cs
+++ src/Spdx/Properties/Usings.cs
@@ -1,9 +1,7 @@
-global using System;
-global using System.Collections.Generic;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Runtime.CompilerServices;
 global using System.Text;
 global using Spdx.Expressions.Ast;
 global using Spdx.Expressions.Parsing;
 
-[assembly: InternalsVisibleTo("Spdx.tests")]
+[assembly: InternalsVisibleTo("Spdx.Tests")]
